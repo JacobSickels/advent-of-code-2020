0c0376b872a963e748e436fd9e5cae7c29733d76
Jacob Sickels <jacob.sickels@singlewire.com>
2020-12-18 19:24:43 -0600

    Day 17

--- src/advent_of_code_2020/core.clj
+++ src/advent_of_code_2020/core.clj
@@ -1,4 +1,5 @@
-(ns advent-of-code-2020.core)
+(ns advent-of-code-2020.core
+  (:require [clojure.math.combinatorics :as combo]))
 
 (defn read-file [file]
   (with-open [reader (clojure.java.io/reader file)]
@@ -6,3 +7,9 @@
 
 (defn xor [& args]
   (not (apply = args)))
+
+
+(defn points-around [point]
+  (remove (fn [p] (= point p))
+          (apply combo/cartesian-product 
+                 (map #(range (dec %) (+ 2 %)) point))))
